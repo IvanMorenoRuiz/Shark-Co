dfe0b8dccf7ec70ae8f23987ea79b6a6963defab
Luca Lusuardi Masip <75097605+lucalm2004@users.noreply.github.com>
2023-11-08 19:33:56 +0100

    Delete pruebalogin directory

--- pruebalogin/hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-$contrasena = "poiPOI123"; // Reemplaza esto con la contraseña que deseas convertir en hash
-
-$hashContraseña = password_hash($contrasena, PASSWORD_DEFAULT);
-
-echo $hashContraseña;
