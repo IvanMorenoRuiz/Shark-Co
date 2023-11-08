4a65e3e68cb66c8c9793c281b31981405610eac7
Luca Lusuardi Masip <75097605+lucalm2004@users.noreply.github.com>
2023-11-08 19:33:40 +0100

    Delete convertidorpwd directory

--- convertidorpwd/cifrado.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$contrasena = $_POST['contrasena']; // Reemplaza esto con la contraseña que deseas convertir en hash
-
-$hashContraseña = password_hash($contrasena, PASSWORD_DEFAULT);
-
-echo $hashContraseña;
-?>
