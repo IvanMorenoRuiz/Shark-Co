3ba9d1ed1bffa4f90d33f57eb7a1ce107f6a605e
IvanMorenoRuiz <134777032+IvanMorenoRuiz@users.noreply.github.com>
2023-11-15 19:53:07 +0100

    Update altaalumnos.php

--- inc/altaalumnos.php
+++ inc/altaalumnos.php
@@ -1,26 +1,22 @@
 <?php
-if (!isset($_POST['num_matricula']) || empty($_POST['num_matricula']) || empty($_POST['dni_alu']) || empty($_POST['nombre_alu']) || empty($_POST['apellido_alu'])) {
-    header('Location: ../alumnos.php');
-    exit;
-}
 
 include_once "conexion.php";
 
 // Obtener los datos del formulario
-$num_matricula = $_POST['num_matricula'];
-$dni_alu = $_POST['dni_alu'];
-$nombre_alu = $_POST['nombre_alu'];
-$apellido_alu = $_POST['apellido_alu'];
+$dni_alu = isset($_POST['dni_alu']) ? $_POST['dni_alu'] : '';
+$nombre_alu = isset($_POST['nombre_alu']) ? $_POST['nombre_alu'] : '';
+$apellido_alu = isset($_POST['apellido_alu']) ? $_POST['apellido_alu'] : '';
+
 
 // Preparar la consulta SQL
-$query = mysqli_prepare($conn, 'INSERT INTO tbl_alumno (num_matricula, dni_alu, nombre_alu, apellido_alu) VALUES (?, ?, ?, ?)');
+$query = mysqli_prepare($conn, 'INSERT INTO tbl_alumno (dni_alu, nombre_alu, apellido_alu) VALUES (?, ?, ?)');
 if ($query === false) {
     // Error en la preparación de la consulta
     echo "Error al preparar la consulta: " . mysqli_error($conn);
     exit;
 }
 
-mysqli_stmt_bind_param($query, 'ssss', $num_matricula, $dni_alu, $nombre_alu, $apellido_alu);
+mysqli_stmt_bind_param($query, 'sss', $dni_alu, $nombre_alu, $apellido_alu);
 
 // Ejecutar la consulta
 if (mysqli_stmt_execute($query)) {
