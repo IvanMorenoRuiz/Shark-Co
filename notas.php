20388fa7df13a09bf2921b91fc65426cd0a17d31
IvanMorenoRuiz <134777032+IvanMorenoRuiz@users.noreply.github.com>
2023-11-15 20:16:30 +0100

    Create notas.php

---
+++ notas.php
@@ -0,0 +1,123 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['dni_prof'])) {
+    header("location: ./index.php");
+    exit;
+} else if (isset($_GET['logout'])) {
+    session_destroy();
+    header("location: ./index.php");
+    exit;
+}
+
+include "./inc/conexion.php"; // Incluye tu archivo de conexión
+
+
+
+$order = "num_matricula";
+if (isset($_GET["order"])) {
+    $order = $_GET["order"];
+}
+
+$nombre = $_GET['nombre_alu'];
+$query1 = "SELECT * FROM tbl_alumno_nota_assignatura";
+$result1 = mysqli_query($conn, $query1); // Utiliza $conn para realizar la consulta
+
+// Verificar si se ha enviado una consulta de búsqueda
+if (isset($_POST['buscar'])) {
+    // Obtener el nombre ingresado en el formulario de búsqueda
+    $nombre = $_POST['nombre'];
+
+    // Realizar la consulta a la base de datos para buscar coincidencias de nombres
+    $query = "SELECT * FROM tbl_alumno WHERE nombre_alu LIKE '%$nombre%' ORDER BY $order";
+    $result1 = mysqli_query($conn, $query); // Utiliza $conn para realizar la consulta
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shark & Co</title>
+    <link rel="shortcut icon" href="./src/LOGO/_55770202-d102-434c-ab15-1b4f4bb9e1a3.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="./stylesnotas.css">
+</head>
+
+<body>
+    <div id="oscuro">
+        <header>
+            <div class="flex headerparte1">
+                <a href="./inc/"><button class="logoutboton"><img class="logoutimg" src="./src/LOGOUT.png" alt=""></button></a>
+                <a href="./alumnos.php"><img class="nav-logo" src="./src/LOGO/LOGO NOMBRE SHARKANDCO.png" alt=""></a>
+            </div>
+            <div class="alta flex">
+                <a href="javascript:history.back()"><button class="atrasboton"><img class="atrasimg" src="./src/atras.png" alt=""></button></a>
+            </div>
+        </header>
+        <div class="alumnos">
+            <!-- TABLA ALUMNOS -->
+            <div id="tablaAlumnos">
+                <div>
+                <h3 id="titulo">Notas de  <?php echo $nombre; ?> </h3>
+
+                </div>
+                <table class="tabla1 separaciones">
+
+                    <thead>
+                        <tr class="noresaltar">
+                            <th class="titulos">Asignatura</th>
+                            <th class="titulos">Notas</th>
+                            <th class="titulos"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        $nombre = ""; 
+                        if ($result1) {
+                            while ($row = mysqli_fetch_assoc($result1)) { // Utiliza mysqli_fetch_assoc
+                                echo "<tr>";
+                                echo "<td class='primerosbordes'>" . $row["id_assignatura"] . "</td>";
+                                echo "<td class='ultimosbordes'>" . $row["nota_alumno"] . "</td>";
+                                echo "<td class='sinfondo nohover'><button id='editar' class='editar'>Editar</button></a></td>";
+                                echo "</tr>";
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- ---------------- -->
+</body>
+
+<script>
+    const btnAlumnos = document.getElementById("btnAlumnos");
+    const btnProfesores = document.getElementById("btnProfesores");
+
+    const tablaAlumnos = document.getElementById("tablaAlumnos");
+    const tablaProfesores = document.getElementById("tablaProfesores");
+
+    tablaAlumnos.style.display = "block";
+    tablaProfesores.style.display = "none";
+
+    btnAlumnos.addEventListener("click", function() {
+        tablaAlumnos.style.display = "block";
+        tablaProfesores.style.display = "none";
+    });
+
+    btnProfesores.addEventListener("click", function() {
+        tablaAlumnos.style.display = "none";
+        tablaProfesores.style.display = "block";
+    });
+</script>
+
+</html>
